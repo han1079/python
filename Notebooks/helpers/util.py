ee10c3acff9fdf41667c7949bac44b487f5dbb18
Han Zhang <han@astranis.com>
2026-04-26 13:13:21 -0700

    updated path search libs for Notebooks

--- Notebooks/helpers/util.py
+++ Notebooks/helpers/util.py
@@ -1,5 +1,13 @@
 import pathlib
 
+_REPO_ROOT = pathlib.Path(__file__).resolve().parent.parent.parent
+
 def extract_asset_paths(subdir: pathlib.Path = pathlib.Path("."), regex: str = "*.*"):
-    path_to_assets = pathlib.Path.home() / "Documents/Notebooks/assets" / subdir
+    path_to_assets = _REPO_ROOT / "Notebooks/assets" / subdir
+    print(f"Searching in {path_to_assets}")
     return list(path_to_assets.rglob(regex))
+
+def extract_cwd_files(regex: str = "*.*"):
+    path_to_files = pathlib.Path.cwd()
+    print(f"Searching in {path_to_files}")
+    return list(path_to_files.rglob(regex))
